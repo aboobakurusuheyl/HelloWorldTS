7ab671f03c7ae0a6fdeeab275fd3c65a9abb0beb
bobsuheyl <sukki17@live.com>
2017-06-17 22:20:28 +0500

    hello

---
+++ HelloWorld.js
@@ -0,0 +1,3 @@
+var uname = "process.argv[2]";
+var message = "Hello World From My First Project";
+console.log(message);
---
+++ HelloWorld.ts
@@ -0,0 +1,3 @@
+let uname:string = process.argv[2];
+let message:string = "Hello World From My First Project";
+console.log(message);
